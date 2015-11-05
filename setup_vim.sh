c5425482d0e8e55b49542fadb273c19c81e60327
Jianfeng <wjfustc@gmail.com>
2015-11-05 04:21:39 -0600

    revise setup vim

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,8 +1,15 @@
-sudo apt-get install -y vim-nox git
+sudo apt-get install -y vim-nox git rake ruby-dev
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+#setup the plugin
 cp .vimrc ~/
 vim +PluginInstall +qall
+
+# setup the ycm
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
+
+#setup the command t
+cd ~/.vim/bundle/command-t
+rake make
