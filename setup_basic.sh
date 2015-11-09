20f1ae41e5865f618238e9d783f0c20879f8736f
Jianfeng <wjfustc@gmail.com>
2015-11-08 20:35:03 -0600

    add ipython config

--- setup_basic.sh
+++ setup_basic.sh
@@ -1,4 +1,9 @@
+# install packages
 sudo apt-get install -y rake
 sudo apt-get install -y ruby-dev
 sudo pip install pyyaml
 sudo pip install lmdb
+
+# setup the ipython configuration 
+ipython profile create 
+echo "c.InteractiveShell.autoindent = False" >> ~/.config/ipython/profile_default/ipython_config.py
