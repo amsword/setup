d9cf49c400cdf0a38878070083311d609a9e9c79
Jianfeng Wang <wjfustc@gmail.com>
2017-05-14 23:09:40 -0700

    improve vim

--- .vimrc
+++ .vimrc
@@ -1,47 +1,18 @@
 set nocompatible              " be iMproved, required
 filetype off                  " required
-
-" set the runtime path to include Vundle and initialize
- set rtp+=~/.vim/bundle/Vundle.vim
- call vundle#begin()
-" " alternatively, pass a path where Vundle should install plugins
-" "call vundle#begin('~/some/path/here')
-"
-" " let Vundle manage Vundle, required
- Plugin 'gmarik/Vundle.vim'
- "Plugin 'flazz/vim-colorschemes'
- Plugin 'mkitt/tabline.vim'
- Plugin 'vim-scripts/Conque-GDB'
- Plugin 'SirVer/ultisnips'
- Plugin 'honza/vim-snippets'
- Plugin 'altercation/vim-colors-solarized'
-"
-" " The following are examples of different formats supported.
-" " Keep Plugin commands between vundle#begin/end.
-" " plugin on GitHub repo
- "Plugin 'klen/python-mode'
- Plugin 'Valloric/YouCompleteMe'
- Plugin 'rdnetto/YCM-Generator'
- Plugin 'oplatek/Conque-Shell'
- Plugin 'amsword/comments.vim'
- "Plugin 'Shougo/neocomplcache.vim'
-
- Plugin 'tpope/vim-fugitive'
-" " plugin from http://vim-scripts.org/vim/scripts.html
-" Plugin 'L9'
-" " Git plugin not hosted on GitHub
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'gmarik/Vundle.vim'
+Plugin 'vim-scripts/Conque-GDB'
+Plugin 'altercation/vim-colors-solarized'
+Plugin 'Valloric/YouCompleteMe'
+Plugin 'rdnetto/YCM-Generator'
+Plugin 'oplatek/Conque-Shell'
+Plugin 'amsword/comments.vim'
+Plugin 'tpope/vim-fugitive'
 Plugin 'git://git.wincent.com/command-t.git'
-" " git repos on your local machine (i.e. when working on your own plugin)
-" Plugin 'file:///home/gmarik/path/to/plugin'
-" " The sparkup vim script is in a subdirectory of this repo called vim.
-" " Pass the path to set the runtimepath properly.
-" Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
-" " Avoid a name conflict with L9
-" Plugin 'user/L9', {'name': 'newL9'}
-"
-" " All of your Plugins must be added before the following line
- call vundle#end()            " required
- filetype plugin indent on    " required
+call vundle#end()            " required
+filetype plugin indent on    " required
 
 
 set wrap
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,29 +1,31 @@
 # based on https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git cmake
 
-sudo apt-get remove vim vim-runtime gvim
+sudo apt-get remove -y vim vim-runtime gvim
 
 git clone https://github.com/vim/vim.git
 cd vim/src
+# the version of python might be bumpped, pls change it accordingly
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp \
             --enable-pythoninterp \
             --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
             --enable-python3interp \
-            --with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
             --enable-perlinterp \
             --enable-luainterp \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim80
 sudo make install
 
-# sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-# sudo update-alternatives --set editor /usr/bin/vim
-# sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-# sudo update-alternatives --set vi /usr/bin/vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -32,17 +34,17 @@ cp .vimrc ~/
 vim +PluginInstall +qall
 
 # install the pre-compiled clang-3.9, which is required by ycm
-wget http://llvm.org/releases/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-tar xf clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-cd clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-sudo cp -R * /usr/local/
+wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+tar xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+cd clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 
 # setup the ycm
 cd ~
 mkdir ycm_build
 cd ycm_build
-cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=/usr/local/ . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake -G "Unix Makefiles" \
+	-DPATH_TO_LLVM_ROOT=~/code/setup/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04 . \
+	~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 cmake --build . --target ycm_core --config Release
 
 #setup the command t
