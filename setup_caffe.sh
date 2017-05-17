07dbf4c4fd4dbf87a48fa52e19596c1481f2d87e
Jianfeng Wang <wjfustc@gmail.com>
2017-05-16 20:30:06 -0700

    add lib in caffe.sh

--- setup_caffe.sh
+++ setup_caffe.sh
@@ -2,13 +2,13 @@ sudo apt-get install -y git
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev
 sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
 sudo apt-get install -y python-numpy python-scipy python-matplotlib python-sklearn python-skimage python-h5py python-protobuf python-leveldb python-networkx python-nose python-pandas python-gflags Cython ipython
+sudo apt-get install -y libhdf5-serial-dev 
 sudo apt-get install -y cmake
 sudo apt-get install -y libatlas-base-dev
 sudo apt-get install -y python-pip
 sudo apt-get install -y rake
-sudo apt-get install -y gdb
-sudo apt-get install -y libclang-dev
+sudo apt-get install -y gdb bc
+#sudo apt-get install -y libclang-dev
 
-sudo apt-get install -y python-pip
 sudo pip install pygments
 sudo pip install django
