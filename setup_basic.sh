4d76b1c0469f00a7217b2b60d117b49e0e9e73a2
Jianfeng Wang <jianfengwang@outlook.com>
2017-06-21 23:04:24 +0000

    a

--- setup_basic.sh
+++ setup_basic.sh
@@ -1,9 +1,9 @@
 set -e
-# install packages
 sudo apt-get update
 sudo apt-get install -y \
+    ipython \
     build-essential \
-    clang
+    clang \
     git \
     ipython \
     lsb-release \
@@ -18,12 +18,11 @@ sudo apt-get install -y \
 sudo locale-gen en_US en_US.UTF-8 hu_HU hu_HU.UTF-8 
 sudo dpkg-reconfigure locales
 
+git config --global user.email "jianfengwang@outlook.com"
+git config --global user.name "Jianfeng Wang"
+
 sudo pip install -r requirements.txt
 
-git config --global user.email "jianfengwang@outlook.com" && \
-        git config --global user.name "Jianfeng Wang"
-
-# setup the ipython configuration 
 ipython profile create 
 mkdir -p ~/.config/ipython/profile_default/
 echo "c.InteractiveShell.autoindent = False" >> ~/.config/ipython/profile_default/ipython_config.py
