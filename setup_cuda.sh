7bf6709bf5a585860caf869e193cd8f94417f3fc
Jianfeng Wang <jianfengwang@outlook.com>
2017-12-01 13:34:19 -0800

    update setup_cuda

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -1,10 +1,15 @@
 apt-get install -y wget
-wget https://amsword.blob.core.windows.net/setup/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb -O cuda-repo.deb
+if [ ! -f cuda-repo.deb ]; then
+    wget https://amsword.blob.core.windows.net/setup/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb -O cuda-repo.deb
+fi
 dpkg -i cuda-repo.deb
 apt-get update
 apt-get install -y cuda
 
 rm -f cuda-repo.deb
 
-wget https://amsword.blob.core.windows.net/setup/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb -O cuda-patch.deb
+if [ ! -f cuda-patch.deb ]; then
+    wget https://amsword.blob.core.windows.net/setup/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb -O cuda-patch.deb
+fi
+
 dpkg -i cuda-patch.deb
