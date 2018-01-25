aba8f3ea8d41400bba1c041939aaba4bffa0d7d6
Jianfeng Wang <jianfengwang@outlook.com>
2018-01-24 16:29:51 -0800

    refine orb slam2 setup

--- setup_orb_slam2.sh
+++ setup_orb_slam2.sh
@@ -1,7 +1,9 @@
 # install Pangolin
+sudo apt-get install libglew-dev
 mkdir -p ~/code/
 cd ~/code
 git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
 git checkout -b for_orb_slam2 e6d000f3e21b0945baa91261dd67fc52f128b049 
 mkdir build
 cd build
