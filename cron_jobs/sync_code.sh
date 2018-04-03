4cada6a12bac5110b6006d013499a3f273ddf213
Jianfeng Wang <jianfengwang@outlook.com>
2018-04-02 18:50:50 -0700

    update sync-code cron-jobs

--- cron_jobs/sync_code.sh
+++ cron_jobs/sync_code.sh
@@ -1,11 +1,11 @@
 date "+%H:%M:%S   %d/%m/%y"
 echo "start\n"
 rsync /home/jianfw/code/quickdetection/* \
-    REDMOND.jianfw@vig-gpu01:/home/jianfw/code/quickdetection_backup \
+    REDMOND.jianfw@vigdgx01:/home/jianfw/code/quickdetection_backup \
     -ravz
 
 rsync /home/jianfw/code/mysite/* \
-    REDMOND.jianfw@vig-gpu01:/home/jianfw/code/mysite_backup/ \
+    REDMOND.jianfw@vigdgx01:/home/jianfw/code/mysite_backup/ \
     -ravz
 
 echo "\ndone"
