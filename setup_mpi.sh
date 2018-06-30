7319c6dc7eab0708fae3e98bf9ede273742ae266
Jianfeng Wang <jianfengwang@outlook.com>
2018-06-29 21:19:50 -0700

    update

--- setup_mpi.sh
+++ setup_mpi.sh
@@ -2,13 +2,16 @@ sudo apt-get install libnuma-dev
 
 mkdir -p ~/data/lib
 cd ~/data/lib
-FILENAME=openmpi-1.10.3
+VERSION=3.1
+FILENAME=openmpi-$VERSION.1
 ZIPFILENAME=$FILENAME.tar.gz
-URL=https://download.open-mpi.org/release/open-mpi/v1.10/$ZIPFILENAME
+URL=https://download.open-mpi.org/release/open-mpi/v$VERSION/$ZIPFILENAME
 wget $URL
 tar -xvf $ZIPFILENAME
 cd $FILENAME
 ./configure --prefix=/usr/local
 make -j
 make all
-make install
+sudo make install
+# update the lib 
+sudo ldconfig
