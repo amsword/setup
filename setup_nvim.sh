403b4de59a3b5d0549b7bbcbdc026cb6e34f9b40
Jianfeng Wang <jianfengwang@outlook.com>
2018-09-21 01:25:57 -0700

    no longer support ubuntu 14 for neovim

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -17,38 +17,17 @@ rm -rf ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
-LSB_RELEASE_INFO=$(lsb_release -r)
-IS_14=1
-if [[ $LSB_RELEASE_INFO = *"16.04" ]]; then
-    IS_14=0
-fi
-
-if [ $IS_14 -eq 1 ]; then
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-get update
-    sudo apt-get install -y gcc-5 g++-5
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 \
-        60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-fi
 
 python generate_vimrc.py
 cd ~/code/setup
 cp .vimrc_plugin ~/.vimrc
 nvim +PluginInstall +qall
 
-if [ $IS_14 -eq 1 ]; then
-    CLANG_FILE_NAME="clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04" 
-    CLANG_TAR_FILE_NAME=${CLANG_FILE_NAME}.tar.xz
-    rm -f $CLANG_TAR_FILE_NAME
-    wget http://releases.llvm.org/4.0.0/$CLANG_TAR_FILE_NAME
-    tar xf $CLANG_TAR_FILE_NAME 
-else
-    CLANG_FILE_NAME="clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04" 
-    CLANG_TAR_FILE_NAME=${CLANG_FILE_NAME}.tar.xz
-    rm -f $CLANG_TAR_FILE_NAME
-    wget http://releases.llvm.org/6.0.1/$CLANG_TAR_FILE_NAME
-    tar xf $CLANG_TAR_FILE_NAME 
-fi
+CLANG_FILE_NAME="clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04" 
+CLANG_TAR_FILE_NAME=${CLANG_FILE_NAME}.tar.xz
+rm -f $CLANG_TAR_FILE_NAME
+wget http://releases.llvm.org/6.0.1/$CLANG_TAR_FILE_NAME
+tar xf $CLANG_TAR_FILE_NAME 
 
 # setup the ycm
 rm -rf ycm_build
@@ -64,3 +43,6 @@ cp ~/code/setup/.vimrc ~/.vimrc
 #setup the command t
 cd ~/.vim/bundle/command-t
 rake make
+
+# nvim with python support. assume conda is used
+pip install neovim
