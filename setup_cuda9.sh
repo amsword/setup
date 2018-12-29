986dfcbee468ce7b1ae80d1bbea7de9475b27211
Jianfeng Wang <jianfengwang@outlook.com>
2018-12-28 20:38:07 -0800

    cuda

---
+++ setup_cuda9.sh
@@ -0,0 +1,26 @@
+CUDA_URL=https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+
+PATCH1_URL=https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64-deb
+PATCH2_URL=https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update-2_1.0-1_amd64-deb
+PATCH3_URL=https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update-3_1.0-1_amd64-deb
+PATCH4_URL=https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda-repo-ubuntu1604-9-0-176-local-patch-4_1.0-1_amd64-deb
+
+wget $CUDA_URL -O cuda.deb
+sudo dpkg -i cuda.deb
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
+
+wget $PATCH1_URL -O patch1.deb
+sudo dpkg -i patch1.deb
+
+wget $PATCH2_URL -O patch2.deb
+sudo dpkg -i patch2.deb
+
+wget $PATCH3_URL -O patch3.deb
+sudo dpkg -i patch3.deb
+
+wget $PATCH4_URL -O patch4.deb
+sudo dpkg -i patch4.deb
+
