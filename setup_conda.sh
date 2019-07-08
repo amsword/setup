ee1ca33cd5cc78d9c1bbe3dacabf5100f14994f9
Jianfeng Wang <jianfengwang@outlook.com>
2019-07-08 09:33:17 -0700

    docker

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,7 +1,12 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+chmod +x Anaconda3-2019.03-Linux-x86_64.sh
+./Anaconda3-2019.03-Linux-x86_64.sh
+
+conda install -y python=3.6
+
+conda install -y ipython
+
+conda install pytorch torchvision cudatoolkit=9.0 -c pytorch
 conda install -y pymongo
 conda install -y -c anaconda libprotobuf protobuf
-conda install -y -c menpo opencv
 conda install -y -c auto easydict
-conda install -y -c intel mkl=2018
-conda install -y -c conda-forge ipdb
-conda install -y -c menpo opencv
